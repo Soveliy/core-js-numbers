097e2331fcda3580c3121053be93940c68b46435
Soveliy <nikita.elagin2012@yandex.ru>
2024-12-05 23:13:43 +0300

    feat: add toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -231,8 +231,11 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Boolean(Number(value)) || Number(value) === 0) {
+    return Number(value);
+  }
+  return def;
 }
 
 /**
