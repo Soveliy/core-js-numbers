5901f2df77ce337fc0aa7ffd2f70bf0cc30b0c1e
Soveliy <nikita.elagin2012@yandex.ru>
2024-12-15 01:00:13 +0300

    feat: add toExponential

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -364,8 +364,9 @@ function numberToStringInBase(number, base) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  const numberExp = number.toExponential(fractionDigits);
+  return numberExp;
 }
 
 /**
